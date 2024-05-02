dbee7b48fc169d5a4307ea815f6b9af6b7f41406
Brian Juma <bjuma0985@gmail.com>
2024-05-02 16:03:50 +0300

    mini-project

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
+import axios from "axios";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys,setToys]=useState([])
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/toys")
+      .then((response) => setToys(response.data));
+  },[]);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
@@ -18,7 +25,7 @@ function App() {
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys} />
     </>
   );
 }
